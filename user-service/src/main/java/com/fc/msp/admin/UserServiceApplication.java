001a1ef130de433c6fb28e7c32543f5a403ac5e2
FangCheng233 <fangcheng828@gmail.com>
2020-07-22 08:30:38 +0800

    $补充pom 文件

---
+++ user-service/src/main/java/com/fc/msp/admin/UserServiceApplication.java
@@ -0,0 +1,18 @@
+package com.fc.msp.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName UserServiceApplication
+ * @Description TODO
+ * @Author fangcheng
+ * @Date 2020/7/22 8:11 上午
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
