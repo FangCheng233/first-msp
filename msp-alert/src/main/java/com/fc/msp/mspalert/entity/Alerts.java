851a61bf21b696a3b8aa84f39a1126104ff9bf20
fangcheng828@gmail.com <44010410+FangCheng233@users.noreply.github.com>
2020-07-27 11:24:20 +0800

    练习用项目

--- msp-alert/src/main/java/com/fc/msp/mspalert/entity/Alerts.java
+++ msp-alert/src/main/java/com/fc/msp/mspalert/entity/Alerts.java
@@ -17,7 +17,7 @@ public class Alerts {
     @Value("${status}")
     private String status;
     @Value("${alerts}")
-    private List<Alert> alertList;
+    private List<Alert> alerts;
     private GroupLabels groupLabels;
     private CommonLabels commonLabels;
     private CommonAnnotations commonAnnotations;
@@ -47,12 +47,12 @@ public class Alerts {
     }
 
     /**
-     * Gets the value of alertList. *
+     * Gets the value of alerts. *
      *
-     * @return the value of alertList
+     * @return the value of alerts
      */
-    public List<Alert> getAlertList() {
-        return alertList;
+    public List<Alert> getAlerts() {
+        return alerts;
     }
 
     /**
@@ -128,12 +128,12 @@ public class Alerts {
     }
 
     /**
-     * Sets the alertList. *
-     * <p>You can use getAlertList() to get the value of alertList</p>
-     * * @param alertList alertList
+     * Sets the alerts. *
+     * <p>You can use getAlerts() to get the value of alerts</p>
+     * * @param alertList alerts
      */
-    public void setAlertList(List<Alert> alertList) {
-        this.alertList = alertList;
+    public void setAlerts(List<Alert> alerts) {
+        this.alerts = alerts;
     }
 
     /**
@@ -195,7 +195,7 @@ public class Alerts {
         return "Alerts{" +
                 "receiver='" + receiver + '\'' +
                 ", status='" + status + '\'' +
-                ", alertList=" + alertList +
+                ", alerts=" + alerts +
                 ", groupLabels=" + groupLabels +
                 ", commonLabels=" + commonLabels +
                 ", commonAnnotations=" + commonAnnotations +
