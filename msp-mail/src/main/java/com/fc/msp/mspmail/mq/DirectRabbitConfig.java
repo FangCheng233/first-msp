9844fc36da88af9560656396ceb0e56d6d2dc847
FangCheng233 <fangcheng828@gmail.com>
2020-07-29 17:00:51 +0800

    $

---
+++ msp-mail/src/main/java/com/fc/msp/mspmail/mq/DirectRabbitConfig.java
@@ -0,0 +1,37 @@
+package com.fc.msp.mspmail.mq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+/**
+ * @ClassName DirectRabbitConfig
+ * @Description TODO
+ * @Author fangcheng
+ * @Date 2020/7/29 4:53 下午
+ * @Version 1.0
+ */
+
+@Configuration
+public class DirectRabbitConfig {
+
+    //队列 起名：TestDirectQueue
+    @Bean
+    public Queue TestDirectQueue() {
+        return new Queue("TestDirectQueue",true);
+    }
+
+    //Direct交换机 起名：TestDirectExchange
+    @Bean
+    DirectExchange TestDirectExchange() {
+        return new DirectExchange("TestDirectExchange");
+    }
+
+    //绑定  将队列和交换机绑定, 并设置用于匹配键：TestDirectRouting
+    @Bean
+    Binding bindingDirect() {
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+}
---
+++ msp-mail/src/main/java/com/fc/msp/mspmail/mq/DirectReceiver.java
@@ -0,0 +1,25 @@
+package com.fc.msp.mspmail.mq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @ClassName DirectReceiver
+ * @Description TODO
+ * @Author fangcheng
+ * @Date 2020/7/29 4:54 下午
+ * @Version 1.0
+ */
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
